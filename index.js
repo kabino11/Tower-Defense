f2f24651946e472bd1e5ae46654c03d8593c79f4
Aditya Mehra <mehraaditya71@gmail.com>
2016-04-23 18:44:50 -0600

    Node Sever is up and running, moved the files/folders into public folder.

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+	http = require('http'),
+	path = require('path'),
+	app = express();
+
+//
+// Define the port to use
+app.set('port', (5000 || 3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/scripts', express.static(__dirname + '/scripts'));
+
+//
+// Define the different routes we support
+app.get('/', function(request, response) {
+	response.sendFile('index.html');
+});
+
+//
+// Get the server created and listening for requests
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Tower defense is running on ' + app.get('port'));
+});
